92fe9eb149e50a813d7ec754f0298b425cd53550
Prathmesh-kale <79739726+Prathmesh-kale@users.noreply.github.com>
2021-06-14 11:11:18 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCN75_rLh-SMpMzjAdZn70vIIRYU_H2mek",
+    authDomain: "c--93-project.firebaseapp.com",
+    projectId: "c--93-project",
+    storageBucket: "c--93-project.appspot.com",
+    messagingSenderId: "948487575428",
+    appId: "1:948487575428:web:82c250984eafb6b4d22fd1"
+  };
+
+  firebase.initializeApp(firebaseConfig);
